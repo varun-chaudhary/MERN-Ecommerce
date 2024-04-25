e7b7b2fa9b2675cbd7ff3aea38830a6a18d890b5
Varun Chaudhary <varunc105@gmail.com>
2024-04-26 02:14:59 +0530

    passport js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
+// "mongodb+srv://varun:varunecom@cluster0.nvzihu3.mongodb.net/ecom"
+
 const express = require("express");
 const server = express();
 const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
 const cors = require("cors");
-
+const LocalStrategy = require("passport-local").Strategy;
+const { User } = require("./model/User");
 const { createProduct } = require("./controller/Product");
 const productsRouter = require("./routes/Products");
 const categoriesRouter = require("./routes/Categories");
@@ -12,14 +17,21 @@ const authRouter = require("./routes/Auth");
 const cartRouter = require("./routes/Cart");
 const ordersRouter = require("./routes/Order");
 
-//middlewares
-
+server.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+server.use(passport.authenticate("session"));
 server.use(
   cors({
     exposedHeaders: ["X-Total-Count"],
   })
 );
-server.use(express.json()); // to parse req.body
+
+server.use(express.json());
 server.use("/products", productsRouter.router);
 server.use("/categories", categoriesRouter.router);
 server.use("/brands", brandsRouter.router);
@@ -28,6 +40,38 @@ server.use("/auth", authRouter.router);
 server.use("/cart", cartRouter.router);
 server.use("/orders", ordersRouter.router);
 
+passport.use(
+  new LocalStrategy(async function (username, password, done) {
+    try {
+      const user = await User.findOne({ email: username }).exec();
+
+      console.log({ user });
+      if (!user) {
+        return done(null, false, { message: "No such Email exists" });
+      } else if (user.password !== password) {
+        return done(null, false, { message: "Wrong Password" });
+      } else {
+        return done(null, user);
+      }
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser(function (user, cb) {
+  console.log("serialize", user);
+  process.nextTick(function () {
+    return cb(null, { id: user.id, role: user.role });
+  });
+});
+
+passport.deserializeUser(function (user, cb) {
+  console.log("de-serialize", user);
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
 main().catch((err) => console.log(err));
 
 async function main() {
